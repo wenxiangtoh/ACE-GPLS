33d4ef12e291a2097d4f3890789322c0f9a6819e
Toh Wen Xiang <wenxiang.toh@ufinity.com>
2021-12-30 17:32:08 +0800

    Initial commit for project skeleton

---
+++ assignment/service-feedback/src/test/java/com/service/feedback/ServiceFeedbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.feedback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceFeedbackApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
