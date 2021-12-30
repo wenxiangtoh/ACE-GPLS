040da9231f1d930f274137f415c196922ee1251a
Toh Wen Xiang <wenxiang.toh@ufinity.com>
2021-12-30 17:51:48 +0800

    Update service-feedback application file.

--- assignment/service-feedback/src/main/java/com/service/feedback/FeedbackApplication.java
+++ assignment/service-feedback/src/main/java/com/service/feedback/FeedbackApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServiceFeedbackApplication {
+public class FeedbackApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(ServiceFeedbackApplication.class, args);
+    SpringApplication.run(FeedbackApplication.class, args);
   }
 
 }
