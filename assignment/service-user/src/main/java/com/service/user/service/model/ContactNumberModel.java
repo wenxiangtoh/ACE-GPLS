83ba094df131401982fe7ef9851dc20acf5559ff
Toh Wen Xiang <wenxiang.toh@ufinity.com>
2021-12-31 15:04:05 +0800

    [service-user] Added lombok annotations to models.

--- assignment/service-user/src/main/java/com/service/user/service/model/ContactNumberModel.java
+++ assignment/service-user/src/main/java/com/service/user/service/model/ContactNumberModel.java
@@ -2,12 +2,20 @@ package com.service.user.service.model;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  * Contact Number Model containing contact information
  *
  * @author Wen Xiang
  */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ContactNumberModel {
   @Positive private Long countryCode;
 
--- assignment/service-user/src/main/java/com/service/user/service/model/CreateUserModel.java
+++ assignment/service-user/src/main/java/com/service/user/service/model/CreateUserModel.java
@@ -3,12 +3,23 @@ package com.service.user.service.model;
 import javax.validation.Valid;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  * Create User Model to create user
  *
  * @author Wen Xiang
  */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
 public class CreateUserModel {
 
   @NotBlank private String name;
