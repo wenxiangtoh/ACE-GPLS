7d0d1ce3c88750946ae8777f9452b2a603438d63
Toh Wen Xiang <wenxiang.toh@ufinity.com>
2021-12-31 12:13:17 +0800

    [service-feedback] Added Feedback Entity and Repository.

--- assignment/service-feedback/src/main/java/com/service/feedback/FeedbackApplication.java
+++ assignment/service-feedback/src/main/java/com/service/feedback/FeedbackApplication.java
@@ -2,12 +2,17 @@ package com.service.feedback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableCaching
+@EntityScan("com.service")
+@EnableJpaRepositories(basePackages = "com.service.feedback.repository")
 @SpringBootApplication
 public class FeedbackApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(FeedbackApplication.class, args);
   }
-
 }
---
+++ assignment/service-feedback/src/main/java/com/service/feedback/repository/FeedbackRepository.java
@@ -0,0 +1,8 @@
+package com.service.feedback.repository;
+
+import com.service.feedback.repository.entity.Feedback;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FeedbackRepository extends JpaRepository<Feedback, Long> {}
