8dce8f9cce2dc8fdb172c2a293af81ad09532a3e
Toh Wen Xiang <wenxiang.toh@ufinity.com>
2022-01-06 23:03:36 +0800

    [service-portal][web-portal] Updated environment variables.

--- assignment/web-portal/src/constants/apiUrl.ts
+++ assignment/web-portal/src/constants/apiUrl.ts
@@ -1,3 +1,3 @@
-export const GET_AGENCIES_URL = `/agencies`;
-export const POST_FEEDBACKS_URL = `/feedbacks`;
-export const POST_FEEDBACK_INFO_URL = `/feedbacks/search`;
+export const GET_AGENCIES_URL = `${process.env.REACT_APP_API_DOMAIN}/api/agencies`;
+export const POST_FEEDBACKS_URL = `${process.env.REACT_APP_API_DOMAIN}/api/feedbacks`;
+export const POST_FEEDBACK_INFO_URL = `${process.env.REACT_APP_API_DOMAIN}/api/feedbacks/search`;
